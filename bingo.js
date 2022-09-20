08465402af49d9cc2eb2cf5855b1ec92131b57e5
Nemephir <jeremyzunino@jeremyzunino.fr>
2022-09-20 18:08:03 +0200

    Test

--- bingo.js
+++ bingo.js
@@ -29,21 +29,11 @@ mongoose.connect( process.env.DB_URL, {
 	useUnifiedTopology: true
 } )
 
-// const allowedOrigins = [ `https://${process.env.TWITCH_EXTENSION_ID}.ext-twitch.tv` ]
+const allowedOrigins = [ `https://${process.env.TWITCH_EXTENSION_ID}.ext-twitch.tv` ]
 // console.log( allowedOrigins )
-// app.use( cors( {
-// 	origin: function( origin, callback ) {
-// 		// allow requests with no origin
-// 		// (like mobile apps or curl requests)
-// 		if( ! origin ) return callback( null, true )
-// 		if( allowedOrigins.indexOf( origin ) === -1 ) {
-// 			var msg = 'The CORS policy for this site does not ' +
-// 				'allow access from the specified Origin.'
-// 			return callback( new Error( msg ), false )
-// 		}
-// 		return callback( null, true )
-// 	}
-// } ) )
+app.use( cors( {
+	origin: allowedOrigins[0]
+} ) )
 
 app.get( '/', ( req, res ) => {
 	res.send( 'ok' )
